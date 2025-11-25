d0a6ed85a99cfdc82eea1ae5e7674a2216f91be2
Anand.N <Anandrandy887@gmail.com>
2025-11-25 15:57:20 +0530

    Fix import path for get_member_by_id function

--- routers/members.py
+++ routers/members.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Path
-from db_models import get_member_by_id
+from db.db_models import get_member_by_id
 
 router = APIRouter(prefix="/members", tags=["Members"])
 
@@ -11,3 +11,4 @@ def get_member(
     if not result:
         raise HTTPException(status_code=404, detail="Member not found")
     return result
+
